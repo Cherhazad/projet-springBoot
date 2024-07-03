f43243de5b58e092faee4469a67029bbefda951b
Cherhazad <cherhazad.boustani@outlook.fr>
2024-07-03 16:20:46 +0200

    TP4 - méthode post dans le contrôleur

--- src/main/java/fr/diginamic/hello/controleurs/VilleController.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleController.java
@@ -1,9 +1,12 @@
 package fr.diginamic.hello.controleurs;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +16,29 @@ import fr.diginamic.hello.entites.Ville;
 @RequestMapping("/villes")
 public class VilleController {
 
-	
-	@GetMapping("/france")
-	public List<Ville> listerVilles(){
-		return List.of(new Ville("Montpellier", 36000), new Ville("Paris", 85000));
+	List<Ville> listeVilles = new ArrayList<>(); //utiliser List.of return une erreur null car une liste est immutable.
+
+	public VilleController() {
+		listeVilles.add(new Ville("Montpellier", 36000));
+		listeVilles.add(new Ville("Paris", 85000));
+	}
+
+	@GetMapping
+	public List<Ville> listerVilles() {
+		return listeVilles;
+	}
+
+	@PostMapping("/insertVille")
+	public ResponseEntity<String> insertVille(@RequestBody Ville nvVille) {
+
+		for (Ville ville : listeVilles) {
+			if (ville.getNom().equals(nvVille.getNom())) {
+
+				return ResponseEntity.badRequest().body("La ville existe déjà");
+			}
+		}
+		listeVilles.add(nvVille);
+		return ResponseEntity.ok("Ville insérée avec succès");
 	}
 
 }
